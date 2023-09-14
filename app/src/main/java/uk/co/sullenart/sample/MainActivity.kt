cde25c289ebe4de4a8f25764b7ef66d509abb0ef
Geoff <geoff.day@matchesfashion.com>
2023-09-14 18:20:51 +0100

    Initial commit

---
+++ app/src/main/java/uk/co/sullenart/sample/MainActivity.kt
@@ -0,0 +1,16 @@
+package uk.co.sullenart.sample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class MainActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent { 
+            Text(text = "Geoff is great!")
+        }
+    }
+}
